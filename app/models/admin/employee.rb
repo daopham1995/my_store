a1fe72fdf1f9f33f12a6250f5a3faa479ec29a50
daopham1995 <daopt22@gmail.com>
2017-10-11 17:28:51 +0700

    static page

---
+++ app/models/admin/employee.rb
@@ -0,0 +1,8 @@
+module Admin
+  class Employee < ApplicationRecord
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :trackable, :validatable
+  end
+end
