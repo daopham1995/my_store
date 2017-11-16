bf343fc58c5ca7d035e08679ab0957c07ee254a4
daopham1995 <daopt22@gmail.com>
2017-11-16 15:00:40 +0700

    fix ci

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,2 @@
 Rails.application.routes.draw do
-  devise_for :employees, class_name: "Admin::Employee"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
