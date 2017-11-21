90e148ad7ad5e82247c807d3e60b477ec0635847
daopham1995 <daopt22@gmail.com>
2017-11-21 17:00:43 +0700

    test employees

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120085445) do
+ActiveRecord::Schema.define(version: 20171121074743) do
 
   create_table "employees", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "name", limit: 50, null: false
+    t.string "email", limit: 255, null: false
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_employees_on_email", unique: true
   end
 
 end
