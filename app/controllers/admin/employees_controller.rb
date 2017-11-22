30eed77b9843dff5c668e172192e2631ec591e96
daopham1995 <daopt22@gmail.com>
2017-11-22 17:08:39 +0700

    sign-in

--- app/controllers/admin/employees_controller.rb
+++ app/controllers/admin/employees_controller.rb
@@ -1,2 +1,13 @@
 class Admin::EmployeesController < ApplicationController
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new
+  end
+
+  def show
+    @employee = Employee.find_by id: params[:id]
+  end
 end
