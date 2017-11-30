0128fa08dc5ea6ed136f36fce124a99d093cf8bc
daopham1995 <daopt22@gmail.com>
2017-11-30 17:23:15 +0700

    new, create employee

--- app/controllers/admin/employees_controller.rb
+++ app/controllers/admin/employees_controller.rb
@@ -4,10 +4,21 @@ class Admin::EmployeesController < ApplicationController
   end
 
   def create
-    @employee = Employee.new
+    @employee = Employee.new(employee_params)
+    if @employee.save
+      redirect_to admin_employee_path(@employee)
+    else
+      render :new, status: 422
+    end
   end
 
   def show
     @employee = Employee.find_by id: params[:id]
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:name, :email, :password)
   end
 end
